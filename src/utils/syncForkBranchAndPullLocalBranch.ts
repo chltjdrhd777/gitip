ccc341944cd89ff1e281ed4b350e6e11ab95d9e8
devAnderson <chltjdrhd777@naver.com>
2024-07-22 08:48:36 +0900

    feat: husky 적용
    개발 컨벤션 git hook 적용을 위해 허스키를 도입하였습니다.

--- src/utils/syncForkBranchAndPullLocalBranch.ts
+++ src/utils/syncForkBranchAndPullLocalBranch.ts
@@ -12,6 +12,15 @@ export default function syncForkBranchAndPullLocalBranch({
   BRANCH_NAME,
   upstreamRemoteAlias,
 }: SyncForkBranchAndPullLocalBranchParams) {
-  executeCommand(`gh repo sync ${FORK_REPO_OWNER}/${REPO_NAME} -b ${BRANCH_NAME}`);
-  executeCommand(`git pull ${upstreamRemoteAlias} ${BRANCH_NAME}`);
+  const executeSync = executeCommand(`gh repo sync ${FORK_REPO_OWNER}/${REPO_NAME} -b ${BRANCH_NAME}`);
+  if (executeSync === null) {
+    console.log('failed to sync repo');
+    throw new Error();
+  }
+
+  const executePull = executeCommand(`git pull ${upstreamRemoteAlias} ${BRANCH_NAME}`);
+  if (executePull === null) {
+    console.log('failed to git pull. please check the change status first');
+    throw new Error();
+  }
 }
