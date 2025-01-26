a8c547ff40412c77913f83ddd5c8086035701b4c
devAnderson <chltjdrhd777@naver.com>
2025-01-27 00:00:48 +0900

    fix: add console for clean branch

--- src/service/github-service/deleteLocalBranches.ts
+++ src/service/github-service/deleteLocalBranches.ts
@@ -1,9 +1,21 @@
-import { executeCommand } from '@/utils';
+import { DefaultConfig } from '@/types';
+import { executeCommand, PROCESS_EXIT } from '@/utils';
 
 export function deleteLocalBranches(localBranchNames: string[]) {
   if (!localBranchNames.length) return;
 
-  localBranchNames.forEach((branchName) => {
-    executeCommand(`git branch -D ${branchName}`, { exitWhenError: false });
-  });
+  try {
+    localBranchNames.forEach((branchName) => {
+      executeCommand(`git branch -D ${branchName}`, {
+        exitWhenError: false,
+        onError: () => deleteLocalBranchesErrorMessage(branchName),
+      });
+    });
+  } catch {
+    PROCESS_EXIT();
+  }
+}
+
+export function deleteLocalBranchesErrorMessage(branchName: string) {
+  console.log(`👀 If your branch is issue branch to delete, please switch to the other branch and try again`);
 }
