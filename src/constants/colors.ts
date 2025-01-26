820ccc54a1751f2af3e39d8937afdf8b388934fb
devAnderson <chltjdrhd777@naver.com>
2025-01-26 23:39:35 +0900

    docs: update README.md for command option update

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,17 +1,28 @@
 const bold = '\x1b[1m';
-const cyan = '\x1b[36m';
+const reset = '\x1b[0m';
+
+const black = '\x1b[30m';
+const red = '\x1b[31m';
 const green = '\x1b[32m';
 const yellow = '\x1b[33m';
+const blue = '\x1b[34m';
 const magenta = '\x1b[35m';
-const reset = '\x1b[0m';
+const cyan = '\x1b[36m';
+const white = '\x1b[37m';
+const gray = '\x1b[90m';
 
 export const COLORS = {
   bold,
   reset,
-  cyan,
+  black,
+  red,
   green,
   yellow,
+  blue,
   magenta,
+  cyan,
+  white,
+  gray,
 };
 
 export const highlighted = (text: string, colors: Array<keyof typeof COLORS> = []) => {
@@ -20,8 +31,13 @@ export const highlighted = (text: string, colors: Array<keyof typeof COLORS> = [
 
 export class ColorCode {
   static bold = (text: string) => `${COLORS.bold}${text}${COLORS.reset}`;
-  static cyan = (text: string) => `${COLORS.cyan}${COLORS.bold}${text}${COLORS.reset}`;
+  static black = (text: string) => `${COLORS.black}${COLORS.bold}${text}${COLORS.reset}`;
+  static red = (text: string) => `${COLORS.red}${COLORS.bold}${text}${COLORS.reset}`;
   static green = (text: string) => `${COLORS.green}${COLORS.bold}${text}${COLORS.reset}`;
   static yellow = (text: string) => `${COLORS.yellow}${COLORS.bold}${text}${COLORS.reset}`;
+  static blue = (text: string) => `${COLORS.blue}${COLORS.bold}${text}${COLORS.reset}`;
   static magenta = (text: string) => `${COLORS.magenta}${COLORS.bold}${text}${COLORS.reset}`;
+  static cyan = (text: string) => `${COLORS.cyan}${COLORS.bold}${text}${COLORS.reset}`;
+  static white = (text: string) => `${COLORS.white}${COLORS.bold}${text}${COLORS.reset}`;
+  static gray = (text: string) => `${COLORS.gray}${COLORS.bold}${text}${COLORS.reset}`;
 }
--- src/constants/command/commandMap.ts
+++ src/constants/command/commandMap.ts
@@ -2,24 +2,24 @@ import { ColorCode } from '../colors';
 import { Command } from './Command';
 import { CommandStore } from './CommandStore';
 
-const { green } = ColorCode;
+const { white } = ColorCode;
 
 /** fork repo command store */
 const forkRepoCommandStore = new CommandStore().addCommands([
-  new Command(`${green('[issue]')} create an issue`, './gitScripts/forkScripts/gitIssue.js'),
-  new Command(`${green('[pr]')} create a pull request`, './gitScripts/forkScripts/gitPR.js'),
+  new Command(`${white('[issue]')} create an issue`, './gitScripts/forkScripts/gitIssue.js'),
+  new Command(`${white('[pr]')} create a pull request`, './gitScripts/forkScripts/gitPR.js'),
   new Command(
-    `${green('[sync]')} synchronize a fork branch with a remote branch`,
+    `${white('[sync]')} synchronize a fork branch with a remote branch`,
     './gitScripts/forkScripts/syncBranch.js',
   ),
-  new Command(`${green('[clean]')} clean issue branches`, './gitScripts/forkScripts/cleanIssueBranches.js'),
+  new Command(`${white('[clean]')} clean issue branches`, './gitScripts/forkScripts/cleanIssueBranches.js'),
 ]);
 
 /** origin repo command store */
 const originRepoCommandStore = new CommandStore().addCommands([
-  new Command(`${green('[issue]')} create an issue`, './gitScripts/originScripts/gitIssue.js'),
-  new Command(`${green('[pr]')} create a pull request`, './gitScripts/originScripts/gitPR.js'),
-  new Command(`${green('[clean]')} clean issue branches`, './gitScripts/originScripts/cleanIssueBranches.js'),
+  new Command(`${white('[issue]')} create an issue`, './gitScripts/originScripts/gitIssue.js'),
+  new Command(`${white('[pr]')} create a pull request`, './gitScripts/originScripts/gitPR.js'),
+  new Command(`${white('[clean]')} clean issue branches`, './gitScripts/originScripts/cleanIssueBranches.js'),
 ]);
 
 export { forkRepoCommandStore, originRepoCommandStore };
